8f23d0986e25a196939649b5c681fad4796a9cb0
Robiul Islam Rubel <77890232+robiul-islam-rubel@users.noreply.github.com>
2023-12-22 21:36:55 +0600

    Create Squared.cpp

---
+++ Squared.cpp
@@ -0,0 +1,36 @@
+/*
+                      Robiul Islam Rubel
+                      Uttara University
+           Depertment of Computer Science & Engineering
+*/
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define read freopen("input.txt","r",stdin);
+using namespace std;
+int main()
+{
+  ll n;
+  cin>>n;
+  cout<<n*n;
+
+
+    /*char c='a';
+    cout<<c;
+    ///s[0]=T s[1]=0......
+   /* for(int i=0;i<s.size();i++)
+    {
+        cout<<s[i]<<endl;
+    }
+    */
+   ///int a=4,b=6;
+   ///scanf("%d%d",&a,&b);
+   ///printf("%d",a+b);
+    /// int float double long double
+    /// string char
+    ///"abbbb"
+    ///'c'
+
+
+    return 0;
+}
