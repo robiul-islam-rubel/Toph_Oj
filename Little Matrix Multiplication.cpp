618d385a4bf197b8a645e99745794015d25d69b9
Robiul Islam Rubel <77890232+robiul-islam-rubel@users.noreply.github.com>
2023-12-22 21:32:53 +0600

    Create Little Matrix Multiplication.cpp

---
+++ Little Matrix Multiplication.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,h;
+    cin>>a>>b>>c>>d>>e>>f>>g>>h;
+    cout<<(a*e)+(b*g)<<" "<<(a*f)+(b*h)<<endl;
+    cout<<(c*e)+(d*g)<<" "<<(c*f)+(d*h)<<endl;
+    return 0;
+}
